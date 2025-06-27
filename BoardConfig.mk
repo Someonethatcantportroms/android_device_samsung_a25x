f90c95b40a2c0523be40aff254af0571a4474f86
ksawlii <ksawery.blaszczak@proton.me>
2025-06-27 20:49:10 +0200

    a53x: Initial NFC support
    Co-authored-by: Jan Altensen <info@stricted.net>
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,8 @@ include device/samsung/s5e8825-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/a53x
 
+# Manifest
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
