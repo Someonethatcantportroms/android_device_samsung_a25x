exynos1280/android_device_samsung_a53x

a53x: Add product build prop overrides
Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
Signed-off-by: ksawlii <ksawery.blaszczak@proton.me>

lineage_a53x.mk
@@ -38,3 +38,9 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 31
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a53xnaxx-user 15 AP3A.240905.015.A2 A536BXXUEFYE2 release-keys" \
+    BuildFingerprint=samsung/a53xnaxx/essi:15/AP3A.240905.015.A2/A536BXXUEFYE2:user/release-keys \
+    DeviceProduct=a53xnaxx \
+    SystemName=a53xnaxx

Someonethatcantportroms/android_device_samsung_a25x

Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

lineage_a53x.mk
@@ -36,3 +36,9 @@ PRODUCT_CHARACTERISTICS := phone
 
 # UDFPS
 TARGET_HAS_UDFPS := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a53xnaxx-user 15 AP3A.240905.015.A2 A536BXXSGFYG1 release-keys" \
+    BuildFingerprint=samsung/a53xnaxx/essi:15/AP3A.240905.015.A2/A536BXXSGFYG1:user/release-keys \
+    DeviceProduct=a53xnaxx \
+    SystemName=a53xnaxx