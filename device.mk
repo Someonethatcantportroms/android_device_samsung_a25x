60039ed0c6b67e2c78f0d08684cf79050d0b7b64
Ksawlii <maja.blaszczak@proton.me>
2025-08-20 23:27:47 +0200

    a53x: props: Move model-specific props to sku
    Change-Id: I77d2c5f2ede6736e8ee8bb0110f041fb35ab72a9
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -60,18 +60,18 @@ $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:
 
 # Variant props
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/props/odm_a53x/odm_SM-A5360.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A5360.prop \
-    $(LOCAL_PATH)/configs/props/odm_a53x/odm_SM-A536B.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A536B.prop \
-		$(LOCAL_PATH)/configs/props/odm_a53x/odm_SM-A536E.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A536E.prop \
-    $(LOCAL_PATH)/configs/props/product_a53x/product_SM-A5360.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A5360.prop \
-    $(LOCAL_PATH)/configs/props/product_a53x/product_SM-A536B.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A536B.prop \
-		$(LOCAL_PATH)/configs/props/product_a53x/product_SM-A536E.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A536E.prop \
-    $(LOCAL_PATH)/configs/props/system_a53x/system_SM-A5360.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A5360.prop \
-    $(LOCAL_PATH)/configs/props/system_a53x/system_SM-A536B.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A536B.prop \
-		$(LOCAL_PATH)/configs/props/system_a53x/system_SM-A536E.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A536E.prop \
-    $(LOCAL_PATH)/configs/props/system_ext_a53x/system_ext_SM-A5360.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A5360.prop \
-    $(LOCAL_PATH)/configs/props/system_ext_a53x/system_ext_SM-A536B.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A536B.prop \
-		$(LOCAL_PATH)/configs/props/system_ext_a53x/system_ext_SM-A536E.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A536E.prop \
-    $(LOCAL_PATH)/configs/props/vendor_a53x/vendor_SM-A5360.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A5360.prop \
-    $(LOCAL_PATH)/configs/props/vendor_a53x/vendor_SM-A536B.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A536B.prop \
-		$(LOCAL_PATH)/configs/props/vendor_a53x/vendor_SM-A536E.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A536E.prop
+    $(LOCAL_PATH)/configs/props/sku/odm_a53x/odm_SM-A5360.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A5360.prop \
+    $(LOCAL_PATH)/configs/props/sku/odm_a53x/odm_SM-A536B.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A536B.prop \
+		$(LOCAL_PATH)/configs/props/sku/odm_a53x/odm_SM-A536E.prop:$(TARGET_COPY_OUT_ODM)/build_SM-A536E.prop \
+    $(LOCAL_PATH)/configs/props/sku/product_a53x/product_SM-A5360.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A5360.prop \
+    $(LOCAL_PATH)/configs/props/sku/product_a53x/product_SM-A536B.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A536B.prop \
+		$(LOCAL_PATH)/configs/props/sku/product_a53x/product_SM-A536E.prop:$(TARGET_COPY_OUT_PRODUCT)/build_SM-A536E.prop \
+    $(LOCAL_PATH)/configs/props/sku/system_a53x/system_SM-A5360.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A5360.prop \
+    $(LOCAL_PATH)/configs/props/sku/system_a53x/system_SM-A536B.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A536B.prop \
+		$(LOCAL_PATH)/configs/props/sku/system_a53x/system_SM-A536E.prop:$(TARGET_COPY_OUT_SYSTEM)/build_SM-A536E.prop \
+    $(LOCAL_PATH)/configs/props/sku/system_ext_a53x/system_ext_SM-A5360.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A5360.prop \
+    $(LOCAL_PATH)/configs/props/sku/system_ext_a53x/system_ext_SM-A536B.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A536B.prop \
+		$(LOCAL_PATH)/configs/props/sku/system_ext_a53x/system_ext_SM-A536E.prop:$(TARGET_COPY_OUT_SYSTEM_EXT)/build_SM-A536E.prop \
+    $(LOCAL_PATH)/configs/props/sku/vendor_a53x/vendor_SM-A5360.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A5360.prop \
+    $(LOCAL_PATH)/configs/props/sku/vendor_a53x/vendor_SM-A536B.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A536B.prop \
+		$(LOCAL_PATH)/configs/props/sku/vendor_a53x/vendor_SM-A536E.prop:$(TARGET_COPY_OUT_VENDOR)/build_SM-A536E.prop
