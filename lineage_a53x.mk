1da7c7794470040a25627d7bbf2098638bfb3580
Majaahh <maja.blaszczak@proton.me>
2025-09-19 15:26:07 +0200

    a53x: Update blobs from A536BXXSHFYI1
    Change-Id: I9acbf9836bfc5aace6ffb7b9f050d712f15b9bb5
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- lineage_a53x.mk
+++ lineage_a53x.mk
@@ -31,7 +31,7 @@ BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_CHARACTERISTICS := phone
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a53xnaxx-user 15 AP3A.240905.015.A2 A536BXXSGFYG1 release-keys" \
-    BuildFingerprint=samsung/a53xnaxx/essi:15/AP3A.240905.015.A2/A536BXXSGFYG1:user/release-keys \
+    BuildDesc="a53xnaxx-user 15 AP3A.240905.015.A2 A536BXXSHFYI1 release-keys" \
+    BuildFingerprint=samsung/a53xnaxx/essi:15/AP3A.240905.015.A2/A536BXXSHFYI1:user/release-keys \
     DeviceProduct=a53xnaxx \
     SystemName=a53xnaxx
